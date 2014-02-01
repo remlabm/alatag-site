b9d44554a6cfcef2efdb3f3bb69b52b95c8a8390
unknown <mbalmer@USF0BF97DD52A0.am.sony.com>
2014-02-01 12:06:00 -0800

    Added email and vendor suppport

---
+++ web/send-email.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+$config = parse_ini_file("../config.ini");
+
+try{
+
+    // Create the Transport
+    $transport = Swift_SmtpTransport::newInstance( $config['smtp']['hostname'], $config['smtp']['port'])
+        ->setUsername( $config['smtp']['user'] )
+        ->setPassword( $config['smtp']['pass'] )
+    ;
+
+    // Create the Mailer using your created Transport
+    $mailer = Swift_Mailer::newInstance($transport);
+
+    // Create a message
+    $message = Swift_Message::newInstance( $_POST['subject'] )
+        ->setFrom( array( $_POST['email'] => $_POST['name'] ))
+        ->setTo( $config['email']['sendTo'] )
+        ->setBody( $_POST['message'] );
+
+    // Send the message
+    $result = $mailer->send($message);
+    $return = ['status' => 'ok' ];
+
+}catch( Exception $e ){
+    $return = ['status' => 'err', 'message' => $e->getMessage() ];
+}
+
+header("Content-Type: application/json");
+echo json_encode( $return );
